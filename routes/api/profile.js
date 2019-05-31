e18c86e23a01df84d87df9e560820c3cdd5604e5
Peace4Every1 <paligyan@gmail.com>
2019-05-31 15:07:27 +0400

    Removing experiences

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -223,4 +223,29 @@ router.put(
   }
 );
 
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.delete("/experience/:exp_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    //Get remove index
+    const removeIndex = profile.experience
+      .map(item => item.id)
+      .indexOf(req.params.exp_id);
+
+    if (removeIndex === -1) {
+      return res.status(400).json({ msg: "Experience doesn't exist" });
+    }
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.send("Server error");
+  }
+});
+
 module.exports = router;
