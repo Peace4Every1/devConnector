9558df92e2d85db067349167c814d32aa5b356e1
Peace4Every1 <paligyan@gmail.com>
2019-06-02 14:38:15 +0400

    Added component with its actions

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { setAlert } from "../../actions/alert";
+import PropTypes from "prop-types";
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -16,7 +19,7 @@ const Register = () => {
   const onSubmit = async e => {
     e.preventDefault();
     if (password !== passwordRepeat) {
-      console.log("Passwords don't match");
+      setAlert("Passwords don't match", "danger", 2000);
     } else {
       console.log("success");
     }
@@ -86,4 +89,11 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { setAlert }
+)(Register);
