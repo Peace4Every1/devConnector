26daf24a41b6b4a179c4c272fc3519f958429c5c
Peace4Every1 <paligyan@gmail.com>
2019-06-03 17:38:05 +0400

    Register action & auth reducer added

--- routes/api/users.js
+++ routes/api/users.js
@@ -25,11 +25,12 @@ router.post(
   ],
   async (req, res) => {
     const errors = validationResult(req);
+    const { name, email, password } = req.body;
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() }); //method that returns the error texts from above
     }
 
-    const { name, email, password } = req.body;
+    // const { name, email, password } = req.body;
 
     try {
       //Check if user exists
