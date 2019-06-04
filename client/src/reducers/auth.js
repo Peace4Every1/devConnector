8fefdf4224697105d259642911b15af46de0d43c
Peace4Every1 <paligyan@gmail.com>
2019-06-04 17:45:23 +0400

    User login & redirect

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -2,7 +2,9 @@ import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
   USER_LOADED,
-  AUTH_ERROR
+  AUTH_ERROR,
+  LOGIN_FAIL,
+  LOGIN_SUCCESS
 } from "../actions/types";
 
 const initState = {
@@ -17,10 +19,12 @@ export default function(state = initState, action) {
 
   switch (action.type) {
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem("token", payload.token);
       return { ...state, ...payload, isAuthenticated: true, loading: false };
     case REGISTER_FAIL:
     case AUTH_ERROR:
+    case LOGIN_FAIL:
       localStorage.removeItem("token");
       return { ...state, token: null, isAuthenticated: false, loading: false };
     case USER_LOADED:
