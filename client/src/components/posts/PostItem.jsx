e83416fe21c708c7917430acbd326b466a66af45
Peace4Every1 <paligyan@gmail.com>
2019-06-18 20:25:16 +0400

    Like/Unlike added

--- client/src/components/posts/PostItem.jsx
+++ client/src/components/posts/PostItem.jsx
@@ -3,8 +3,11 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import Moment from "react-moment";
 import { connect } from "react-redux";
+import { addLike, removeLike } from "../../actions/post";
 
 const PostItem = ({
+  addLike,
+  removeLike,
   auth,
   post: { _id, text, name, avatar, user, likes, comments, date }
 }) => (
@@ -21,11 +24,19 @@ const PostItem = ({
         <p className="post-date">
           Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
         </p>
-        <button type="button" className="btn btn-light">
+        <button
+          onClick={() => addLike(_id)}
+          type="button"
+          className="btn btn-light"
+        >
           <i className="fas fa-thumbs-up" />
           {likes.length > 0 && <span>{likes.length}</span>}
         </button>
-        <button type="button" className="btn btn-light">
+        <button
+          onClick={() => removeLike(_id)}
+          type="button"
+          className="btn btn-light"
+        >
           <i className="fas fa-thumbs-down" />
         </button>
         <Link to={`/post/${_id}`} className="btn btn-primary">
@@ -39,41 +50,6 @@ const PostItem = ({
             <i className="fas fa-times" />
           </button>
         )}
-      </div>
-    </div>
-
-    <div className="post bg-white p-1 my-1">
-      <div>
-        <a href="profile.html">
-          <img
-            className="round-img"
-            src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200"
-            alt=""
-          />
-          <h4>John Doe</h4>
-        </a>
-      </div>
-      <div>
-        <p className="my-1">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint possimus
-          corporis sunt necessitatibus! Minus nesciunt soluta suscipit nobis.
-          Amet accusamus distinctio cupiditate blanditiis dolor? Illo
-          perferendis eveniet cum cupiditate aliquam?
-        </p>
-        <p className="post-date">Posted on 04/16/2019</p>
-        <button type="button" className="btn btn-light">
-          <i className="fas fa-thumbs-up" />
-          <span>4</span>
-        </button>
-        <button type="button" className="btn btn-light">
-          <i className="fas fa-thumbs-down" />
-        </button>
-        <a href="post.html" className="btn btn-primary">
-          Discussion <span className="comment-count">3</span>
-        </a>
-        <button type="button" className="btn btn-danger">
-          <i className="fas fa-times" />
-        </button>
       </div>
     </div>
   </div>
@@ -90,5 +66,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  {}
+  { addLike, removeLike }
 )(PostItem);
