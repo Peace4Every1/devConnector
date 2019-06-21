58236972854e047d2e2f76160a068813ca3f859d
Peace4Every1 <paligyan@gmail.com>
2019-06-21 14:37:44 +0400

    Fixed the footer error on main page

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import setAuthToken from "./util/setAuthToken";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
-import Alert from "./components/layout/Alert";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
-import Dashboard from "./components/dashboard/Dashboard";
-import CreateProfile from "./components/profile-forms/CreateProfile";
-import EditProfile from "./components/profile-forms/EditProfile";
-import AddExperience from "./components/profile-forms/AddExperience";
-import AddEducation from "./components/profile-forms/AddEducation";
-import Profiles from "./components/profiles/Profiles";
-import Profile from "./components/profile/Profile";
-import Posts from "./components/posts/Posts";
-import Post from "./components/post/Post";
-import PrivateRoute from "./components/routing/PrivateRoute";
 import { loadUser } from "./actions/auth";
 //Redux
 import { Provider } from "react-redux";
 import store from "./store";
 
 import "./App.css";
+import WithContainer from "./WithContainer";
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -35,42 +23,11 @@ const App = () => {
   return (
     <Provider store={store}>
       <Router>
-        <Fragment>
-          <Navbar />
+        <Navbar />
+        <Switch>
           <Route exact path="/" component={Landing} />
-          <section className="container">
-            <Alert />
-            <Switch>
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/profiles" component={Profiles} />
-              <Route exact path="/profile/:id" component={Profile} />
-              <PrivateRoute exact path="/dashboard" component={Dashboard} />
-              <PrivateRoute
-                exact
-                path="/create-profile"
-                component={CreateProfile}
-              />
-              <PrivateRoute
-                exact
-                path="/edit-profile"
-                component={EditProfile}
-              />
-              <PrivateRoute
-                exact
-                path="/add-experience"
-                component={AddExperience}
-              />
-              <PrivateRoute
-                exact
-                path="/add-education"
-                component={AddEducation}
-              />
-              <PrivateRoute exact path="/posts" component={Posts} />
-              <PrivateRoute exact path="/posts/:id" component={Post} />
-            </Switch>
-          </section>
-        </Fragment>
+          <Route component={WithContainer} />
+        </Switch>
       </Router>
     </Provider>
   );
